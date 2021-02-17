78fddc8477235a0df3317ed81671d3e82a282682
Dan Erdelt <erd@conceptual.ca>
2021-02-17 01:25:38 +0000

    ...

---
+++ src/hooks/version/apply.ts
@@ -0,0 +1,20 @@
+import { Hook, load } from '@oclif/config'
+
+export default async function (opts: any) {
+  console.log(opts.config.loadPlugins);
+  process.stdout.write(`apply v hook ${opts.version}\n`);
+}
+
+/*
+const hook: Hook<'init'> = async function (opts) {
+  opts.config.loadPlugins()
+  process.stdout.write(`example hook running custom\n`)
+}
+*/
+
+/*await config.loadPlugins(path, "user", [{
+    type: "user", // plugin type, user seems best, I have also seen "core" and "link" for npm-linked plugins
+    name: "@my/plugin", // npm name
+    root: "/some/path/node_modules/@my/plugin" // path to plugin code
+}]);
+*/
