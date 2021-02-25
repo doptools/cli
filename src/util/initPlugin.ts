296bc8d0646d6940544fe003b87e4bdbca32df98
Dan Erdelt <erd@conceptual.ca>
2021-02-25 16:53:44 +0000

    removed runtime package deps

--- src/util/initPlugin.ts
+++ src/util/initPlugin.ts
@@ -1,63 +1,65 @@
+
+// Cannot use dependancies here. this will run before the package manager has installed them
+
 import type { PJSON } from '@oclif/config';
-import { jsonc } from 'jsonc';
-import Path from 'path';
-import { findUpPackagePath } from 'resolve-package-path';
 import type { PackageJson } from 'type-fest';
+//import { jsonc } from 'jsonc';
+//import { findUpPackagePath } from 'resolve-package-path';
+import Path from 'path';
+import { readFileSync, writeFileSync, existsSync } from 'fs'
+
+function readSync(path: string): PackageJson {
+    return JSON.parse(readFileSync(path, { encoding: 'utf-8' }));
+}
+function writeSync(path: string, data: any): void {
+    writeFileSync(path, JSON.stringify(data, undefined, 4));
+}
+
+function findUpPackagePath(path: string): string {
+    while(true){
+        const pkg = Path.join(path, 'package.json');
+        if(existsSync(pkg)){
+            return pkg;
+        }
+        if(!path){
+            return '';
+        }
+        path = Path.dirname(path);
+    }
+}
 
 export function initPlugins() {
     let changed = false;
     try {
         const pkgSearchPath = Path.normalize(Path.join(process.cwd(), '..'));
-        console.log('pkgSearchPath   :', pkgSearchPath);
         const pkgPath = findUpPackagePath(pkgSearchPath);
         if (pkgPath != null) {
-            console.log('pkgPath         :', pkgPath);
-            const pkg = jsonc.readSync(pkgPath) as PackageJson & { dops: any };
-            console.log('pkg             :', pkg);
-
+            const pkg = readSync(pkgPath) as PackageJson & { dops: any };
             if (Array.isArray(pkg.dops?.plugins)) {
                 const cliPkgSearchPath = Path.normalize(Path.join(process.cwd()));
-                console.log('cliPkgSearchPath:', cliPkgSearchPath);
                 const cliPkgPath = findUpPackagePath(cliPkgSearchPath);
-                const cliPkg = jsonc.readSync(cliPkgPath) as PackageJson & { dops: any } & PJSON.CLI;
+                const cliPkg = readSync(cliPkgPath) as PackageJson & { dops: any } & PJSON.CLI;
                 const cliPlugins: Array<string | PJSON.PluginTypes.User | PJSON.PluginTypes.Link> = cliPkg.oclif.plugins = cliPkg.oclif.plugins ?? [];
                 const cliDeps = cliPkg.dependencies = cliPkg.dependencies ?? {};
-                
-                var plugins: PJSON.PluginTypes.User[] = pkg.dops.plugins;
-                console.log('plugins         :', plugins);
 
+                var plugins: PJSON.PluginTypes.User[] = pkg.dops.plugins;
                 plugins.forEach(async plugin => {
-                    console.log('plugin         :', plugin);
                     const installedPlugin = cliPlugins.find(p => typeof p === 'string' ? p === plugin.name : p.name == plugin.name);
-                    console.log('installedPlugin', installedPlugin);
                     if (!installedPlugin) {
                         changed = true;
                         cliPlugins.push(plugin);
-                        
+
                     }
-                    if(!cliDeps[plugin.name]){
+                    if (!cliDeps[plugin.name]) {
                         changed = true;
                         cliDeps[plugin.name] = plugin.tag!;
                     }
-                    // if (installedPlugin) {
-
-                    //  } else {
-                    //   cliPkg.dependencies ??= {};
-                    //    changed = true;
-                    //if (cliPkg.dependencies[plugin.url!]) {
-                    //const curVer =  semver.parse pkg.dependencies[plugin.url!];
-                    //     return;
-                    //  }
-                    //   cliPkg.dependencies[plugin.url!] = plugin.tag!;
-                    //    }
                 });
                 if (changed) {
-                    console.log('changed!!!!!!!!!');
-                    jsonc.writeSync(cliPkgPath, cliPkg, { space: 2 });
+                    writeSync(cliPkgPath, cliPkg);
                     // Shell.exec('yarn', { cwd: Path.dirname(cliPkgPath) });
                 }
                 return changed;
-
             }
         }
     } catch (e) {
