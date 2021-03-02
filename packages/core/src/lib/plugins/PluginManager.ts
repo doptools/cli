5ec9356ccb245c90b3b5a3efc09682d83c23a42d
Dan Erdelt <erd@conceptual.ca>
2021-03-02 12:33:48 +0000

    null checks

--- packages/core/src/lib/plugins/PluginManager.ts
+++ packages/core/src/lib/plugins/PluginManager.ts
@@ -29,7 +29,7 @@ export class PluginManager {
             return {
                 type: binPkg.oclif.plugins
                     ?.filter(_ => typeof _ === 'string' ? _ === p.name : _.name === p.name)
-                    ?.map(_ => typeof _ === 'string' ? (_ === '@doptools/cli' ? 'main' : 'core') : _.type )[0] ?? 'main',
+                    ?.map(_ => typeof _ === 'string' ? (_ === '@doptools/cli' ? 'main' : 'core') : _.type)[0] ?? 'main',
                 name: p.name,
                 version: p.version,
                 valid: p.valid
@@ -57,12 +57,14 @@ export class PluginManager {
     }
 
     public async syncPlugins() {
-        if(this.context.contextType !== 'global'){
-            const contextPkg = await jsonc.read(this.context.contextPackageJsonPath!) as PackageJson & IDopsConfig;
-            const binPkg = await jsonc.read(join(this.context.binPath!, 'package.json')) as PJSON.User;
-            binPkg.oclif.plugins ??= [];
-            binPkg.oclif.plugins = binPkg.oclif.plugins.filter(p => typeof p === 'string' ? true : p.type !== 'user');
-    
+        if (this.context.contextType !== 'global') {
+            return this.syncGlobalPlugins();
+        }
+        const contextPkg = await jsonc.read(this.context.contextPackageJsonPath!) as PackageJson & IDopsConfig;
+        const binPkg = await jsonc.read(join(this.context.binPath!, 'package.json')) as PJSON.User;
+        binPkg.oclif.plugins ??= [];
+        binPkg.oclif.plugins = binPkg.oclif.plugins.filter(p => typeof p === 'string' ? true : p.type !== 'user');
+        if (contextPkg.dops?.plugins) {
             for (const name in contextPkg.dops.plugins) {
                 if (Object.prototype.hasOwnProperty.call(contextPkg.dops.plugins, name)) {
                     const tag = contextPkg.dops.plugins[name];
@@ -76,7 +78,6 @@ export class PluginManager {
             await jsonc.write(join(this.context.binPath!, 'package.json'), binPkg);
         }
     }
-
 
     private async savePluginConfig(path: string, pluginName: string, version?: string | null) {
         const pkg = await jsonc.read(path) as PackageJson & IDopsConfig;
@@ -95,6 +96,10 @@ export class PluginManager {
         await jsonc.write(path, pkg, { space: 4 });
     }
 
+    private async syncGlobalPlugins() {
+        
+    }
+
     private async addGlobalPlugin(plugin: string) {
         const info = NodeUtil.packageInfo(plugin);
     }
@@ -102,7 +107,4 @@ export class PluginManager {
     private async removeGlobalPlugin(plugin: string) {
         const info = NodeUtil.packageInfo(plugin);
     }
-
-
-
 }
